6e38803a20b4a97727cfdb065c5efc552363e298
Matheus <ps.matheusb@gmail.com>
2024-08-11 15:46:54 -0300

    configurações de conexão com DB

--- server/config/config.go
+++ server/config/config.go
@@ -1,10 +1,37 @@
 package config
 
-import "gorm.io/gorm"
+import (
+	"fmt"
+
+	"gorm.io/gorm"
+)
 
 var (
 	db *gorm.DB
 )
+
+func Init() error {
+	var err error
+
+	host := "localhost"
+	user := "postgres"
+	password := "1234"
+	dbName := "gaart"
+	port := "5432"
+	sslmode := "disable"
+	timezone := "America/Fortaleza"
+
+	db, err = InitializePostgres(host, user, password, dbName, port, sslmode, timezone)
+	if err != nil {
+		return fmt.Errorf("Error ao inicializar o banco: %v", err)
+	}
+
+	return nil
+}
+
+func GetDB() *gorm.DB {
+	return db
+}
 
 func GetLogger(p string) *Logger {
 	logger := NewLogger(p)
