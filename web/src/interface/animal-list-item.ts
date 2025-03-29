6ba946908851c5aac7015376bb905c29f2ad9fba
matheus.unix <ps.matheusb@gmail.com>
2025-03-29 17:50:59 -0300

    :fire: refract web project

--- web/src/interface/animal-list-item.ts
+++
@@ -1,9 +0,0 @@
-export interface AnimalListItemInterface {
-    nome: string;
-    especie: string;
-    raca: string;
-    porte: string;
-    sexo: number;
-    castrado: boolean;
-    vacinado: boolean;
-}
--- web/src/interface/icon-button.ts
+++
@@ -1,5 +0,0 @@
-export interface IconButtonInterface{
-    icon: string //url do icone
-    label: string //texto que aparece ao passar o mouse
-    onClick?: () => void //função que será executada ao clicar no botão
-}
